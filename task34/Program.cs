23ba45a6932858380639f5c388c0e607dd248da2
«SimLove» <simonolyuba@yandex.ru>
2023-06-09 16:31:01 +0300

    add task34

---
+++ task34/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+int[] GetMass(int length)
+{
+    Random rnd = new Random();
+    int[] massive = new int[length];
+    Console.Write("Полученный массив: ");
+    for(int i=0;i<length;i++)
+    {
+        massive[i]=rnd.Next(100,999);
+        Console.Write(""+massive[i]+" ");
+        if(i==(length-1))
+            Console.Write("\r\n");
+    }
+    return massive;
+}
+
+void CountChet(int[] massive)
+{
+    int count=0;
+    for(int i=0;i<massive.Length;i++)
+        if((massive[i]%2)==0)
+            count++;
+    Console.WriteLine("Количество четных чисел: "+count);
+}
+
+Console.WriteLine("Введите количество элементов массива: ");
+CountChet(GetMass(Convert.ToInt32(Console.ReadLine())));
