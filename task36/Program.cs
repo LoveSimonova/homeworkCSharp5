a89e5841a3b9f105aef6afe9833ef8e57ed1a0c7
«SimLove» <simonolyuba@yandex.ru>
2023-06-09 16:40:24 +0300

    add task36

---
+++ task36/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+int[] GetMass(int length)
+{
+    Random rnd = new Random();
+    int[] massive = new int[length];
+    Console.Write("Массив: ");
+    for(int i=0;i<length;i++)
+    {
+        massive[i]=rnd.Next(0,999);
+        Console.Write(""+massive[i]+" ");
+        if(i==(length-1))
+            Console.Write("\r\n");
+    }
+    return massive;
+}
+
+void Sum(int[] massive)
+{
+    int sum=0;
+    for(int i=0;i<massive.Length;i++)
+        if((i%2)!=0)
+            sum+=massive[i];
+    Console.WriteLine("Сумма: "+sum);
+}
+
+Console.WriteLine("Введите количество элементов массива: ");
+Sum(GetMass(Convert.ToInt32(Console.ReadLine())));
